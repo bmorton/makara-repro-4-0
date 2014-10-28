b7492b4eb6048effd312dd655d48f46834acf73b
Brian Morton <bmorton@yammer-inc.com>
2014-10-27 23:39:48 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakaraRepro40::Application.config.secret_key_base = 'dab41fe3f45f597083a2527d9ea2cbd26cc95fec7de476cc8b17c99f53dfbc1214689186765f4bbaee14f8c833d5341897b7b7d4abe512366fde73870a70c4b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakaraRepro40::Application.config.session_store :cookie_store, key: '_makara-repro-4-0_session'
